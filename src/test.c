359c384f8ff4438d15c1a53cf3f3cd1d6fa6786d
spsandwichman <73349893+spsandwichman@users.noreply.github.com>
2024-07-12 11:28:31 -0400

    almost for the bishps

--- src/test.c
+++ src/test.c
@@ -23,6 +23,27 @@ u64 rook_attack_board(u8 index) {
     return ~rook & ((col << index_col) | (row << (index_row*8)));
 }
 
+u64 bishop_attack_board(u8 index) {
+    u8 index_col = index % 8;
+    u8 index_row = index / 8;
+
+    u64 bishop = 1ll << index;
+
+    int left_distance = (int)index_col - (int)index_row;
+
+    u64 left  = 0x8040201008040201ull;
+    u64 right = 0x0102040810204080ull;
+
+    if (left_distance > 0) {
+        left = left << left_distance;
+    } else {
+        left = left >> -left_distance;
+    }
+    return left;
+    
+    // return ~rook & ((col << index_col) | (row << (index_row*8)));
+}
+
 int main() {
     Board b;
     init_board(&b);
@@ -30,7 +51,10 @@ int main() {
     MoveSet ms = {};
     da_init(&ms, 10);
 
-    print_bitboard(rook_attack_board(19));
+    u8 index = 8;
+
+    print_bitboard(1ll << index); printf("\n");
+    print_bitboard(bishop_attack_board(index));
 
     generate_moves(&b, &ms);
 }
